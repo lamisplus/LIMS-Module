83ed468ad343f141059a279d972a2ff513ec9981
gichangi <gichangijohn3@gmail.com>
2022-09-05 18:30:37 +0300

    initiation

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,6 +1,8 @@
-//export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-//export const url = '/api/v1/'
+export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+export const url = '/api/v1/'
+/*
 export const url =  'http://localhost:8282/api/v1/';
 export const apiUrl =  'http://localhost:8282/api/';
 export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYyMDYwMDIxfQ.lHPN7Jzb0VZvCKT5PuOp_C6ycAWwc5mia8RLVD4n2DzwBqVqJ529DsDjZDKtmayThAcderMTvexGdfkWqXg0YA';
 
+*/
