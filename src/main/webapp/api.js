c4d0ca79991a51a1da82d573f7a6f7903286506f
gichangi <gichangijohn3@gmail.com>
2022-09-07 17:18:46 +0300

    pull request

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,8 +1,8 @@
-export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-export const url = '/api/v1/'
-/*
+/*export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+export const url = '/api/v1/'*/
+
+
 export const url =  'http://localhost:8282/api/v1/';
 export const apiUrl =  'http://localhost:8282/api/';
-export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYyMTQ5NDk3fQ.cB2V3X5Hdopy-1EWPnohjHEWl1l0yavc_orSYDnLViamZOfHVvSBJwwulNBxIpaExzEBax3j3P1uPvVEfPRbYA';
+export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYyNjAzMzY0fQ.oxa5miGmWT6d36f4hBRTKv7gmBh4diBSXst3NCuxzSVL7v0cbclWBW6103Qk9ymACG1qvBXPpS9M8OkDAMZs5w';
 
-*/
