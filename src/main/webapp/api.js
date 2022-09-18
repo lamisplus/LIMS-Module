4eb54ebc7f0c9566a02cf8c7bd27815b02adcd97
vixxjy <ajorvictor48@gmail.com>
2022-09-18 10:16:40 +0100

    [LIMS] manfestlist update

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -6,5 +6,5 @@ export const url = '/api/v1/'
 
 export const url =  'http://localhost:8282/api/v1/';
 export const apiUrl =  'http://localhost:8282/api/';
-export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYzMzcyNjM2fQ.W52NcB5Tlpj-gnX7LuRUpeovO6bPpKG4Ew1hE2HFYWOqaItOUFHR6Ug5A7jSSmGC4OLKs6d_CbwCDhJU6yZ-lw';
+export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYzNTMxNDQ5fQ.7bo7BK_jFDsAiJcLQJuuGGJhKrf4YqgsnMt4YajNF0nJlKesIBvTCOpsUurwp9mhg-ZfADIXgr1miOUF0t3Uug';
 
