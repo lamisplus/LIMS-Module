625308e82c9fe595f42cc94cfdc2dab5d9d97865
vixxjy <ajorvictor48@gmail.com>
2022-09-22 01:31:58 -0700

    [LIMS] git recent update.

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -2,5 +2,5 @@
 //export const url = '/api/v1/'
 export const url =  'http://localhost:8282/api/v1/';
 export const apiUrl =  'http://localhost:8282/api/';
-export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYzMjEyMzkxfQ.dIhLRNrEyCG3qg32TCErBHGLNQ-WHFIq6Yb8mVTgrzREGeRMfYmykJnegU3UT0ZvtOZ2Zr0SGy69RkobDk0xqA';
+export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYzODc0ODA2fQ.JrvPkchXplaiEkUXN7Thbo0hUEvSBoooU0h_pa6I3gPfICrby9cTr2OWgvEvxxmtIBnhEWfYcaaW02KRuq9Xsw';
 
