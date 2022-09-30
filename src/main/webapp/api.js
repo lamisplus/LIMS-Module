41c8a8764732a242b2b8b01d9989cef34a718a69
vixxjy <ajorvictor48@gmail.com>
2022-09-30 11:13:33 +0100

    [LIMS] dates results validation updates

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,6 +1,9 @@
-//export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-//export const url = '/api/v1/'
+/*
+export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+export const url = '/api/v1/'
+*/
+
 export const url =  'http://localhost:8282/api/v1/';
 export const apiUrl =  'http://localhost:8282/api/';
-export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYzNTY4MjY0fQ.93a8K0AR4oRYM7cY_aZ1zouPF_0aEFbv_Zn2V-ltAuirvOop1UewccbKVHQiE6b1Q002pgARYgJGPBygO85xJA';
+export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjY0NTczMTQzfQ.xMM0h0iEGaLdenrXKkNMcE2chwUhCdOAk0oawtau9ybDCRc_X2P-kTr2I1GLeCwy1b6aOrrJx2mpZxXmcKstKw';
 
