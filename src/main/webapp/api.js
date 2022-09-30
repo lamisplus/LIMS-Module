8b7046391dcb132cdc4018764323cd11826c47a1
vixxjy <ajorvictor48@gmail.com>
2022-09-30 11:22:23 +0100

    [LIMS] dates results updates

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -6,4 +6,3 @@ export const url = '/api/v1/'
 export const url =  'http://localhost:8282/api/v1/';
 export const apiUrl =  'http://localhost:8282/api/';
 export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjY0NTczMTQzfQ.xMM0h0iEGaLdenrXKkNMcE2chwUhCdOAk0oawtau9ybDCRc_X2P-kTr2I1GLeCwy1b6aOrrJx2mpZxXmcKstKw';
-
