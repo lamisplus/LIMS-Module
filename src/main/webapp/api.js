c01cb62b829fa7ffd2e5c43eff48e4c2ec07e08f
vixxjy <ajorvictor48@gmail.com>
2022-10-04 00:42:55 -0700

    [LIMS] updates for add test result

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,8 +1,9 @@
-/*
+
 export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 export const url = '/api/v1/'
-*/
 
+/*
 export const url =  'http://localhost:8282/api/v1/';
 export const apiUrl =  'http://localhost:8282/api/';
 export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjY0Nzc5MzE4fQ.MpLZBLo2SzBk-iC4cUJ-VuXkafNhPDSFh2VX-UnhN7ZMVJ55FEXooE_3f520puThzR00P8S119sQsjcb_9rpow';
+*/
