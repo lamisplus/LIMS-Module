18814dcf8ea5c538cd6270e6b6d296ffcf48e6d1
vixxjy <ajorvictor48@gmail.com>
2023-01-09 16:02:16 +0100

    [LIMS] updates for LIMS configurations

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,9 +1,10 @@
-/*
+
 export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 export const url = '/api/v1/'
-*/
 
 
+/*
 export const url =  'http://localhost:8282/api/v1/';
 export const apiUrl =  'http://localhost:8282/api/';
 export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjczMzA4MjIwfQ.mgj50rD-COPKTpQ-XXI4W9-_51ZKzNG3qdTQIpkF4y_AH86aHB2oCrgoMlVwWGBLHoc-xvlvUvoaE-IVhefoyA';
+*/
