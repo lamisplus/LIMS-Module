f2ce85835f22483a18ac1fa083da25972d82dc98
vixxjy <ajorvictor48@gmail.com>
2023-03-24 15:26:23 +0100

    lims updates

---
+++ src/main/webapp/api.js
@@ -0,0 +1,9 @@
+/*
+export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+export const url = '/api/v1/'
+*/
+
+export const url = "http://localhost:8282/api/v1/";
+export const apiUrl = "http://localhost:8282/api/";
+export const token =
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc5NTM0Nzc4fQ.5yzFX_MkSr7uUzzv48HksMCojWrv3yjVbqPuOcKI-oeBi7WvC9iv1GoeDeShSIxuihTIO-mVVi7Qv6d5gPatpQ";
